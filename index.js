2bf950db9cd3baaa349ccc778a630c8958dabb3e
verdeja05 <al222310416@gmail.com>
2026-05-20 20:01:44 -0600

    hola mundo en localhost

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("comit6");
+const express = require('express')
+const app = express();
+app.get('/',(req, res) => {
+    res.send('hola mundo');
+});
+
+app.listen(3000, () => {
+    console.log('escucha desde el puerto 3000');
+});
