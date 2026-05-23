630605718ebb71f0fc014aaab348ba26bd98e443
verdeja05 <al222310416@gmail.com>
2026-05-22 20:58:19 -0600

    commit hola mundo localhost puerto 4000

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('express')
+const express = require ('express')
+
 const app = express();
-app.get('/',(req, res) => {
-    res.send('hola mundo');
+require('dotenv').config();
+
+app.get ('/', (req, res) =>{
+    res.send('Hola mundo');
 });
 
-app.listen(3000, () => {
-    console.log('escucha desde el puerto 3000');
+const PORT = process.env.PORT;
+app.listen(4000, () => {
+    console.log(`escucha desde el puerto ${PORT}`);
+
 });
